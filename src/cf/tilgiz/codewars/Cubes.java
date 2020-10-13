3cee6f20b4493d88fb6b88eb461d81d609cb0acf
tilgiz <4ilgiz@gmail.com>
2020-10-13 16:57:52 +0300

    Hidden "Cubic" numbers

---
+++ src/cf/tilgiz/codewars/Cubes.java
@@ -0,0 +1,11 @@
+package cf.tilgiz.codewars;
+
+public class Cubes {
+    public String isSumOfCubes(String s) {
+
+
+//        return "... To Do... Lucky";
+//		else return "Unlucky";
+        return "";
+    }
+}
--- src/cf/tilgiz/codewars/Main.java
+++ src/cf/tilgiz/codewars/Main.java
@@ -640,9 +640,15 @@ public class Main {
 
 
 
-        System.out.println(Arrays.toString(Walker.solve(12,20,18,45,30,60)));
-        System.out.println(Arrays.toString(Walker.solve(15,15,19,50,29,55)));
-        System.out.println(Arrays.toString(Walker.solve(14,25,17,41,35,59)));
+//        System.out.println(Arrays.toString(Walker.solve(12,20,18,45,30,60)));
+//        System.out.println(Arrays.toString(Walker.solve(15,15,19,50,29,55)));
+//        System.out.println(Arrays.toString(Walker.solve(14,25,17,41,35,59)));
+
+
+        String s = "0 9026315 -827&()";
+        Cubes c = new Cubes();
+        System.out.println(c.isSumOfCubes(s));
+
     }
 
 }
